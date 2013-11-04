f811394a5665ede3d4390e99dee82e2f5c5a652e
JT Paasch <jt.paasch@gmail.com>
2013-11-04 06:00:34 -0500

    Fixed backprop algorithm.

--- apps/brain/computation.py
+++ apps/brain/computation.py
@@ -24,9 +24,7 @@ class Computation:
         # Run the inputs through the network to get the outputs.
         outputs = Computation.activate_network(net, inputs)
 
-        return outputs
-
-        # Return the output layer neurons that lit up.
+        # Find the output layer neurons that lit up.
         top_level = len(outputs) - 1
         top_layer = outputs[top_level]
         results = []
@@ -67,7 +65,7 @@ class Computation:
         return layers
 
     @staticmethod
-    def train(layers, outputs=[], learning_rate=1):
+    def backprop(layers, outputs=[], learning_rate=1):
 
         for layer, neurons in reversed(list(enumerate(layers))):
             is_input_layer = layer is 0
@@ -80,6 +78,7 @@ class Computation:
                     for i, node in enumerate(layers[layer - 1]):
                         link = Select.link_between(node, neuron)
                         link.weight += ((neuron.error * node.output) * learning_rate)
+                        link.save()
             elif is_top_layer and not is_input_layer:
                 for index, neuron in enumerate(neurons):
                     error_sum = 0
@@ -91,9 +90,17 @@ class Computation:
                     for i, node in enumerate(layers[layer - 1]):
                         link = Select.link_between(node, neuron)
                         link.weight += ((neuron.error * node.output) * learning_rate)
+                        link.save()
 
         return layers
 
+    @staticmethod
+    def train(layers, inputs, outputs, iterations=10):
+        for i in range(0, iterations):
+            for j, input in enumerate(inputs):
+                layers = Computation.activate_network(layers, inputs[j])
+                layers = Computation.backprop(layers, outputs[j])
+        return layers
 
     @staticmethod
     def sum(outputs, weights, bias=0):
